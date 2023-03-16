011b98105a505384dfc0acafc9cf34bbcca8d182
imadosrikos <imadosrikos@gmail.com>
2023-03-16 15:51:41 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,17 @@
 #!/bin/bash
 #include <stdio.h>
+#include <stdio.h>
 
-int main() {
+int main () {
 
-  puts("\n\"Programming is like building a multilingual puzzle");
 
-  return 0;
+    // initializing strings
+    char str1[] = "\n\"Programming is like building a multilingual puzzle";
 
-   }
+
+    // writing to stdout
+    puts(str1);
+
+
+
+        return 0;
