1e60b0f4530d22591b1006e48516f77d325051d6
imadosrikos <imadosrikos@gmail.com>
2023-03-16 16:59:03 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,12 @@
 /**
  * main - main function
  *
- * Return: 0
+ * Return: always 0
  */
 
-int main(void)
+int main (void)
 {
   puts ("\"Programming is like building a multilingual puzzle");
-  return(0);
+  return (0);
 }
-
 
