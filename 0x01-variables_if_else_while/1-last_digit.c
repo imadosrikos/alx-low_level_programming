112008f0b767c82fe6c54d00a561c9a79623b041
imadosrikos <imadosrikos@gmail.com>
2023-03-20 14:58:22 -0700

    program will assign random random to the variable n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,13 +5,13 @@
 /* betty style doc for function main goes there */
 /**
 * main - main function
-* 
+*
 * Return: alwayse 0 (success)
 */
+
 int main(void)
 {
-int n;
-
+  int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if ((n % 10) == 0)
