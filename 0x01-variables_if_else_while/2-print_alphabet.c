c183bc2a36189e12f723b314e8d5223e7e17720b
imadosrikos <imadosrikos@gmail.com>
2023-03-20 19:19:23 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - function main
+ *
+ *Return: alwayse 0
+ */
+int main (void)
+{
+char i;
+
+for (i = 'a' ; i <= 'z'; i++);
+putchar(i);
+putchar('\n');
+return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,3 @@
 a program that assigns a random number to the variable n each time it is executed
 a program that will assign a random number to the variable n
+the alphabete game
